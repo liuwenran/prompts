3055e815c906b8437e639de56fb56f6b02d0238a
刘文然 <PJLAB\liuwenran@shai14001335l.pjlab.org>
2023-05-10 16:47:35 +0800

    trans from file

--- translate_file.py
+++ translate_file.py
@@ -7,6 +7,7 @@ import urllib
 import random
 import json
 from pip._vendor.distlib.compat import raw_input
+import time
  
 id_key = open('resources/appid_secretkey.txt', 'r').readlines()
 id = id_key[0].strip()
@@ -19,29 +20,45 @@ secretKey = key
 httpClient = None
 myurl = 'https://fanyi-api.baidu.com/api/trans/vip/translate'  # 通用翻译API HTTP地址
  
-fromLang = 'auto'       # 原文语种
+fromLang = 'en'       # 原文语种
 toLang = 'zh'           # 译文语种
-salt = random.randint(32768, 65536)
-# 手动录入翻译内容，q存放
-q = raw_input("please input the word you want to translate:")
-sign = appid + q + str(salt) + secretKey
-sign = hashlib.md5(sign.encode()).hexdigest()
-myurl = myurl + '?appid=' + appid + '&q=' + urllib.parse.quote(q) + '&from=' + fromLang + \
-        '&to=' + toLang + '&salt=' + str(salt) + '&sign=' + sign
- 
-# 建立会话，返回结果
-try:
-    httpClient = http.client.HTTPConnection('api.fanyi.baidu.com')
-    httpClient.request('GET', myurl)
-    # response是HTTPResponse对象
-    response = httpClient.getresponse()
-    result_all = response.read().decode("utf-8")
-    result = json.loads(result_all)
- 
-    print (result)
- 
-except Exception as e:
-    print (e)
-finally:
-    if httpClient:
-        httpClient.close()
+
+prompt_file = 'english/drawbench.txt'
+results_file = 'chinese/drawbench.txt'
+
+prompts = open(prompt_file, 'r').readlines()
+
+results = []
+httpClient = None
+for prompt in prompts:
+
+    salt = random.randint(32768, 65536)
+    # 手动录入翻译内容，q存放
+    q = prompt.strip()
+    sign = appid + q + str(salt) + secretKey
+    sign = hashlib.md5(sign.encode()).hexdigest()
+    myurl = myurl + '?appid=' + appid + '&q=' + urllib.parse.quote(q) + '&from=' + fromLang + \
+            '&to=' + toLang + '&salt=' + str(salt) + '&sign=' + sign
+    
+    # 建立会话，返回结果
+    try:
+        httpClient = http.client.HTTPConnection('api.fanyi.baidu.com')
+        httpClient.request('GET', myurl)
+        # response是HTTPResponse对象
+        response = httpClient.getresponse()
+        result_all = response.read().decode("utf-8")
+        result = json.loads(result_all)
+        result = result['trans_result'][0]['dst']
+        print (result)
+
+        result += '\n'
+        results.append(result)
+        time.sleep(1)
+    except Exception as e:
+        import pdb;pdb.set_trace();
+        print (e)
+
+open(results_file, 'w').writelines(results)
+
+if httpClient:
+    httpClient.close()
